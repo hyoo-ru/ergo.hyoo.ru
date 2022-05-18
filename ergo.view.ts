241e4ae025d5601800e8347c5dd813705d59aa95
Pavel Zubkov <zubkov_p@mail.ru>
2022-05-18 21:35:24 +0300

    save

--- ergo.view.ts
+++ ergo.view.ts
@@ -2,13 +2,24 @@ namespace $.$$ {
 
 	export class $hyoo_ergo extends $.$hyoo_ergo {
 
+		@ $mol_mem
+		count(next?: number) {
+			return next ?? 0
+		}
+
+		rerender() {
+			this.count( this.count() + 1 )
+		}
+
 		state_holding() {
+			console.log('State holding')
 			this.domain().index().record('qwe123qwe123qwezzxc_', ['123'])
 			this.domain().index().search('hello')
 			return this.domain().index().state()
 		}
 
 		auto() {
+			console.log('Auto')
 			this.state_holding()
 		}
 	}
