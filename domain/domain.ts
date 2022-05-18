75308bbd26133b965f4f3136ceb927c0dfa2b18c
Pavel Zubkov <zubkov_p@mail.ru>
2022-05-18 21:28:09 +0300

    only index and auto

--- domain/domain.ts
+++ domain/domain.ts
@@ -8,60 +8,9 @@ namespace $ {
 			return new $mol_state_shared
 		}
 
-		@ $mol_mem_key
-		person(id: string) {
-			const obj = new this.$.$hyoo_ergo_person
-			obj.id = $mol_const(id)
-			obj.domain = $mol_const(this)
-			return obj
-		}
-
-		@ $mol_mem_key
-		thesis(id: string) {
-			const obj = new this.$.$hyoo_ergo_thesis
-			obj.id = $mol_const(id)
-			obj.domain = $mol_const(this)
-			return obj
-		}
-
-		@ $mol_mem_key
-		request(id: string) {
-			const obj = new this.$.$hyoo_ergo_request
-			obj.id = $mol_const(id)
-			obj.domain = $mol_const(this)
-			return obj
-		}
-
-		@ $mol_mem_key
-		edition(id: string) {
-			const obj = new this.$.$hyoo_ergo_edition
-			obj.id = $mol_const(id)
-			obj.domain = $mol_const(this)
-			return obj
-		}
-
-		@ $mol_mem_key
-		comment(id: string) {
-			const obj = new this.$.$hyoo_ergo_comment
-			obj.id = $mol_const(id)
-			obj.domain = $mol_const(this)
-			return obj
-		}
-
-		@ $mol_mem
-		user() {
-			
-			let id = this.$.$mol_store_local.value( 'user' ) as string | null
-			if( !id ) {
-				id = $mol_guid()
-				new $mol_after_tick( ()=> this.$.$mol_store_local.value( 'user', id ) )
-			}
-			
-			return this.person( id )
-		}
-
 		@ $mol_mem
 		index() {
+			console.log('index instance')
 			const obj = new this.$.$hyoo_ergo_index
 			obj.domain = $mol_const(this)
 			return obj
--- ergo.view.ts
+++ ergo.view.ts
@@ -2,43 +2,6 @@ namespace $.$$ {
 
 	export class $hyoo_ergo extends $.$hyoo_ergo {
 
-		user() {
-			return this.domain().user()
-		}
-
-		user_id() {
-			return this.user().id()
-		}
-
-		@ $mol_mem
-		user_page_auto_open() {
-			// if (!this.user_name_defined()) this.user_page_open()
-			return true
-		}
-
-		thesis_new() {
-			return this.$.$mol_state_arg.value('thesis_new') === ''
-		}
-
-		thesis_arg() {
-			return this.$.$mol_state_arg.value('thesis')
-		}
-
-		thesis() {
-			return this.domain().thesis( this.thesis_arg()! )
-		}
-
-		pages() {
-
-			return [
-				this.Index_page(),
-				... this.User_page().arg() ? [this.User_page()] : [],
-				... this.thesis_new() ? [this.Create_page()] : [],
-				... this.thesis_arg() ? [this.Thesis_page()] : [],
-			]
-
-		}
-
 		state_holding() {
 			this.domain().index().record('qwe123qwe123qwezzxc_', ['123'])
 			this.domain().index().search('hello')
